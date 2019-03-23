da461b96079ae5311b031b569572965ef1b4bb75
Laurent-PANEK <laurent.panek@ynov.com>
2019-03-23 18:51:24 +0100

    fix variable name

--- save.d/engines/mysql.sh
+++ save.d/engines/mysql.sh
@@ -9,9 +9,9 @@ fi
                 
 for db in $(mysql -u${MYSQL_USER} -p${MYSQL_PASSWORD} -e 'show databases;' | grep -Ev "^(Database|mysql|information_schema|performance_schema|phpmyadmin|sys)$")
 do
-    echo "[$($logdate)] processing ${db}"
+    echo "[$($LOGDATE)] processing ${db}"
     mysqldump --opt -u${MYSQL_USER} -p${MYSQL_PASSWORD} "${db}" | gzip > ${WORKING_DIR}/databases/mysql/${db}_$(date +%F).sql.gz
 done
 
-echo "[$($logdate)] processing all db"
+echo "[$($LOGDATE)] processing all db"
 mysqldump --opt -u${MYSQL_USER} -p${MYSQL_PASSWORD} --events --ignore-table=mysql.event --all-databases | gzip > ${WORKING_DIR}/databases/mysql/all_databases_$(date +%F).sql.gz
