e08681ad55b473ef18c8d3ec3b68d333eab91647
Laurent-PANEK <laurent.panek@ynov.com>
2019-03-23 18:21:13 +0100

    add date in output

--- save.d/engines/mysql.sh
+++ save.d/engines/mysql.sh
@@ -9,9 +9,9 @@ fi
                 
 for db in $(mysql -u${MYSQL_USER} -p${MYSQL_PASSWORD} -e 'show databases;' | grep -Ev "^(Database|mysql|information_schema|performance_schema|phpmyadmin|sys)$")
 do
-    echo "processing ${db}"
+    echo "[$($logdate)] processing ${db}"
     mysqldump --opt -u${MYSQL_USER} -p${MYSQL_PASSWORD} "${db}" | gzip > ${WORKING_DIR}/databases/mysql/${db}_$(date +%F).sql.gz
 done
 
-echo "processing all db"
+echo "[$($logdate)] processing all db"
 mysqldump --opt -u${MYSQL_USER} -p${MYSQL_PASSWORD} --events --ignore-table=mysql.event --all-databases | gzip > ${WORKING_DIR}/databases/mysql/all_databases_$(date +%F).sql.gz
--- save.sh
+++ save.sh
@@ -9,15 +9,15 @@ ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 FILE="${ROOT}/$(basename "${BASH_SOURCE[0]}")"
 BASE="$(basename ${FILE}.sh)"
 curday=$(date +%F)
-logfile="/var/log/backup_mega_${curday}"
+logdate="date -Iseconds"
 
-echo "Import config"
+echo "[$($logdate)] Import config"
 
 source ${ROOT}/save.conf
 
 ##################################
 # Create local working directory and collect all data
-echo "Create working directory: ${WORKING_DIR}"
+echo "[$($logdate)] Create working directory: ${WORKING_DIR}"
 
 rm -rf ${WORKING_DIR}
 mkdir ${WORKING_DIR}
@@ -26,7 +26,7 @@ cd ${WORKING_DIR}
 # Backup Database
 if [ "${BACKUP_DB}" = "true" ]
 then
-    echo "Backup DB"
+    echo "[$($logdate)] Backup DB"
     mkdir ${WORKING_DIR}/databases
     for engine in ${DB_ENGINE_TO_BACKUP[*]}
     do
@@ -35,21 +35,21 @@ then
             mkdir ${WORKING_DIR}/databases/${engine}
             source "${ROOT}/save.d/engines/${engine}.sh"
         else
-            echo ${engine} is not supported
+            echo "[$($logdate)] ${engine} is not supported"
         fi
     done
-    echo "Databases saved succesfully"
+    echo "[$($logdate)] Databases saved succesfully"
 fi
 
 
 # Backup Service configuration
 if [ "${BACKUP_SERVICES}" = "true" ]
 then
-    echo "Backup services"
+    echo "[$($logdate)] Backup services"
     mkdir ${WORKING_DIR}/services
     for service in ${SERVICES_TO_BACKUP[*]}
     do
-        echo "processing ${service}"
+        echo "[$($logdate)] processing ${service}"
         if test -f "${ROOT}/save.d/services/${service}.sh"
         then
             source "${ROOT}/save.d/services/${service}.sh"
@@ -59,20 +59,20 @@ then
                 cd /etc
                 tar czf ${WORKING_DIR}/services/${service}.tar.gz ./${service}
             else
-                echo ${service} is not supported
+                echo "[$($logdate)] ${service} is not supported"
             fi
         fi
     done
-    echo "Services saved succesfully"
+    echo "[$($logdate)] Services saved succesfully"
 fi
 
 
-echo "Backup folders"
+echo "[$($logdate)] Backup folders"
 # Backup folders
 mkdir ${WORKING_DIR}/folders
 for backup_folder in ${FOLDERS_TO_BACKUP[*]}
 do
-    echo "Backup ${backup_folder}"
+    echo "[$($logdate)] Backup ${backup_folder}"
     filter_folder=${backup_folder//\//_}
     cd ${backup_folder}
     if [ "${ONLY_SUBFOLDERS}" = "true" ]
@@ -80,7 +80,7 @@ do
         mkdir ${WORKING_DIR}/folders/${filter_folder}
         for folder in $(find ${backup_folder} -mindepth 1 -maxdepth 1 -type d)
         do
-                echo "processing ${folder}"
+                echo "[$($logdate)] processing ${folder}"
                 tar czf ${WORKING_DIR}/folders/${filter_folder}/$(basename ${folder}).tar.gz ./$(basename ${folder})
         done
     else
@@ -88,22 +88,23 @@ do
     fi
 done
 
-echo "Folders saved succesfully"
+echo "[$($logdate)] Folders saved succesfully"
 
-echo "Create backup archive"
+echo "[$($logdate)] Create backup archive"
 
 cd ${WORKING_DIR}
 tar czf /tmp/backup_${SERVER}_${curday}.tar.gz .
 
-echo "Copy latest backup"
+echo "[$($logdate)] Copy latest backup"
 rclone --progress copy /tmp/backup_${SERVER}_${curday}.tar.gz ${REMOTE_NAME}:backup/${SERVER}
 
-echo "Remove old backup"
+echo "[$($logdate)] Remove old backup"
 rclone --dry-run --min-age ${DAYS_TO_BACKUP}d delete ${REMOTE_NAME}:backup/${SERVER}
 rclone --min-age ${DAYS_TO_BACKUP}d --progress delete ${REMOTE_NAME}:backup/${SERVER}
 
-echo "Clean local environment"
+echo "[$($logdate)] Clean local environment"
 rm -rf ${WORKING_DIR} /tmp/backup_${SERVER}_${curday}.tar.gz
 
-echo "Backup done"
+echo "[$($logdate)] Backup done"
+
 exit 0
